026596b180ec9895e1d3bd53a8128aa4c4c50962
Harsimranjeet <pragra9009@gmail.com>
2021-03-10 11:28:20 -0500

    My First project commit

---
+++ tests/calculate.js
@@ -0,0 +1,46 @@
+let homepage = require('../pages/homepage')
+
+describe("Demo Calculator Fucntion Test", function () {
+
+    it("Addition Test", function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo();
+
+        homepage.verfyResult('7');
+
+        browser.sleep(3000);
+
+    });
+
+    it("Subtraction Test", function () {
+
+        homepage.get('http://juliemr.github.io/protractor-demo/');
+
+        homepage.enterFirstNumber('4');
+
+        homepage.enterSecondNumber('3');
+
+        homepage.clickGo1();
+
+        homepage.verfyResult('7');
+
+        browser.sleep(3000);
+
+    });
+
+
+});
+
+
+
+
+
+
+
+  
